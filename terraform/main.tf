3be49fd5b983ddf03cc154a396980aef58b9e4f0
EC2 Default User <ec2-user@ip-172-31-1-93.ap-south-1.compute.internal>
2026-04-02 05:55:46 +0000

    Added Terraform code and gitignore

---
+++ terraform/main.tf
@@ -0,0 +1,38 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id      = aws_vpc.main.id
+  cidr_block  = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id      = aws_vpc.main.id
+  cidr_block  = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id  = aws_vpc.main.id
+}
+
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "cloud-platform-cluster"
+  cluster_version = "1.27"
+
+  vpc_id     = aws_vpc.main.id
+  subnet_ids = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t2.micro"]
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
+    }
+  }
+}
